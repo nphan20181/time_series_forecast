ee700dbb625564e9795dd3cdc99912a6093c542f
Ngoc Phan <nphan20181@gmail.com>
2023-03-10 00:17:19 -0600

    Update es_model.py

---
+++ module/constants.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+# set default labels
+X_LABEL ='Date'
+Y_LABEL ='Weekly Sales (Million)'
+T_LABEL = 'Time Period'
+
+# name of target variable according to transformation method
+TARGET = {'':'Weekly Sales (Million)', 
+          'Log': 'Log of Weekly Sales (Million)',
+          'Sqrt': 'Square Root of Weekly Sales (Million)'}
+
+# name of evaluation metrics
+METRIC_NAMES = {'MAPE': 'Mean Absolute Percentage Error (MAPE)',
+                'MAD': 'Mean Absolute Deviation (MAD)',
+                'RMSE': 'Root Mean Square Error (RMSE)'}
+
+# transformation function
+TRANSFORM = {'Log': np.log, 'Sqrt': np.sqrt}
+REVERSE_TRANSFORM = {'Log': np.exp, 'Sqrt': np.square}
+TRANSFORM_LABEL = {'': 'None', 'Log': 'Logarithm', 'Sqrt': 'Square Root'}
+
+NUMBER_ORDER = {0: 'Zeroth', 1: 'First', 2: 'Second', 3: 'Third', 4: 'Fourth', 5: 'Fifth', 
+                6: 'Sixth', 7: 'Seventh', 8: 'Eighth', 9: 'Ninth', 10: 'Tenth'}
+
+COLORS = {'Observe':'blue', 'Train':'#009933', 'Test':'red', 'Forecast':'#e68a00'}
+
+# z-value for computing 95% confidence interval
+Z = {'.025': 1.96}
---
+++ module/es_model.py
@@ -0,0 +1,116 @@
+import module.constants as const
+import module.util_functions as utf
+import datetime
+import numpy as np
+import pandas as pd
+
+class ExponentialSmoothing:
+    
+    def __init__(self, smoothing_constant=0.5, transform='none'):
+        self.alpha = smoothing_constant                 # set smoothing constant
+        self.observe_label = const.Y_LABEL              # set observe label
+        self.target = const.TARGET.get(transform)       # get label for target variable
+        self.transform_method = transform               # set transformation method
+        self.forecast_label = 'Exponential Smoothing Forecast'
+    
+    def compute_smoothed_series(self, initial_level, Y):
+        '''
+        Compute exponentially smoothed series.
+        '''
+        
+        # create a list of exponetially smoothed series
+        E = [initial_level]
+        
+        # compute exponentially smoothed series and append the result to list E
+        for y_t in Y.values:
+            E.append(self.alpha * y_t + (1 - self.alpha) * E[-1])
+        
+        self.exponential_series = E  # save smoothed series
+    
+    def fit(self, ts_data: pd.DataFrame):
+        '''
+        Train the model.
+        
+        Parm:
+          - ts_data: a time series data frame.
+        '''
+        
+        # duplicate original data
+        data = ts_data.copy()
+        
+        # compute exponentially smoothed series, use the average sale of week 52 as initial level
+        self.compute_smoothed_series(data[data.Week == 52][self.target].mean(), data[self.target])
+        
+        # compute rolling forecasts
+        data[self.forecast_label] = utf.inverse_transform(pd.Series(self.exponential_series[:-1]), self.transform_method)
+        data['Forecast Error'] = data[self.observe_label] - data[self.forecast_label]
+        
+        # compute standard error
+        self.standard_error = np.sqrt(sum(data['Forecast Error']**2) / (data.shape[0] - 1))
+        
+        # save data
+        self.data = data
+        
+    
+    def predict_intervals(self, n_periods: int) -> pd.DataFrame:
+        '''
+        Compute 95% prediction intervals.
+        '''
+        
+        lower = []   # forecast's lower bound
+        upper = []   # forecast's upper bound
+        
+        # get the last value of smoothed series
+        level = utf.inverse_transform(self.exponential_series[-1], self.transform_method)    
+        conf_interval = const.Z.get('.025') * self.standard_error
+        
+        # compute confident intervals
+        for n in range(1, n_periods + 1):
+            if n == 1:
+                lower.append(level - conf_interval)
+                upper.append(level + conf_interval)
+            elif n == 2:
+                lower.append(level - conf_interval * np.sqrt(1 + self.alpha**2))
+                upper.append(level + conf_interval * np.sqrt(1 + self.alpha**2))
+            else:
+                lower.append(level - conf_interval * np.sqrt(1 + (n - 1) * self.alpha**2))
+                upper.append(level + conf_interval * np.sqrt(1 + (n - 1) * self.alpha**2))
+        
+        return pd.DataFrame({'Lower Bound': lower, 'Upper Bound': upper})
+    
+    def predict(self, n_periods=52) -> pd.DataFrame:
+        '''
+        Make constant forecast for future periods.
+        '''
+        
+        # get the last sales date in the data
+        current_date = self.data.Date.iloc[-1]
+        
+        # get a list of future dates for predictions
+        date_list = []
+        for i in range(0, n_periods):
+            current_date = current_date + datetime.timedelta(days=7)
+            date_list.append(current_date)
+        
+        # create forecast data frame
+        df_forecast = pd.concat([pd.DataFrame({'Date': date_list}), 
+                                 pd.DataFrame({'Future Forecast': [self.data[self.forecast_label].values[-1]]*n_periods}), 
+                                 self.predict_intervals(n_periods)], axis=1)
+        
+        return df_forecast
+    
+    def evaluate(self) -> pd.DataFrame:
+        '''
+        Return evaluation metrics on Test data.
+        '''
+        
+        # evaluate model using Test data
+        mape, mad, rmse = utf.compute_metrics(self.data[self.observe_label], self.data[self.forecast_label])
+        
+        # create metrics data frame
+        df_metrics = pd.DataFrame({'Transform': [const.TRANSFORM_LABEL.get(self.transform_method)], 
+                                   'Alpha': [self.alpha],
+                                   'MAPE': [mape], 'MAD': [mad], 'RMSE': [rmse]})
+        
+        return df_metrics
+    
